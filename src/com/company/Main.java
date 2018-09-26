1175e199ac89901dce6118245bf8a96216d80d5c
BT_1N3_19@BKTECH.LOCAL <BT_1N3_19@BKTECH.LOCAL>
2018-09-26 09:14:14 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    String str1 = "LoveJava";
+    while(str1.length()>0)
+    {
+        System.out.println(str1);
+        str1 = str1.substring(1,str1.length()-1);
+    }
+    String str2 = "What?";
+    for(int i = 0; i < str2.length(); i++)
+    {
+        System.out.println(str2);
+        str2 = str2.substring(str2.length()-1) + str2.substring(0,str2.length()-1);
+    }
+    }
+}
